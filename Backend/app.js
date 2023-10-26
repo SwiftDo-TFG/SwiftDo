fb44256090b3fecc258b5fc8380a2a740c8e7898
Pabluskagamo <pabluskagamo@gmail.com>
2023-10-26 19:19:40 +0200

    Retocado app.js y añadido pool de bbdd por configurar

--- Backend/app.js
+++ Backend/app.js
@@ -1,17 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const port = 3000;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,7 +20,7 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.send("Not Found sorry :(");
 });
 
 // error handler
@@ -37,5 +33,13 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(port, function(err){
+  if(err){
+    console.log("Error openning server");
+  }else{
+    console.log(`App listening on port ${port}`)
+  }
+})
 
 module.exports = app;
