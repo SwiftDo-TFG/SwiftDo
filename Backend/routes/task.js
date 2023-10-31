6e1478918a96ccf16186a2329eed2b2b5554f627
CGLMaster <cgomez22@ucm.es>
2023-10-31 18:09:55 +0100

    Create Task (Con dudas a resolver)

--- Backend/routes/task.js
+++ Backend/routes/task.js
@@ -2,5 +2,31 @@ const express = require('express');
 const router = express.Router();
 const taskService = require('../services/taskService')
 
+let task = {
+    user_id: 1,
+    context_id: 1,
+    project_id: 1,
+    title: "Crear tarea",
+    description: "Intento de crear una nueva tarea para meter en la BD",
+    state: "Initiate",
+    verification_list: "",
+    important_fixed: "true",
+    date_added: Date.now(),
+    date_completed: Date.now(),
+    date_limit: Date.now(),
+    date_changed: Date.now(),
+    num_version: 1
+}
+
+router.post('/newTask', async (req, res)=>{
+    try{
+        // const data_task = req.body;
+        const t = taskService.createTask(task);
+        res.send(t);
+    }catch(err){
+        console.log('[Exception]:',err.message)
+        res.sendStatus(404);
+    }
+})
 
 module.exports = router;
--- Backend/services/taskService.js
+++ Backend/services/taskService.js
@@ -2,4 +2,34 @@ const db = require('../bd/pool.js')
 
 const taskService = {}
 
+taskService.createTask = async (task)=>{
+    await db.query('SELECT user_id FROM tasks WHERE user_id = $1', [task.user_id], (err, res)=>{
+        if(err){
+            throw new Error('Connection error with the BD')
+        }
+        else{
+            if(res.rows.length != 0){
+                throw new Error('The task just exist');
+            }
+            else{
+                db.query("INSERT INTO tasks(user_id, context_id, project_id, title, description, state, verification_list, important_fixed, date_added, date_completed, date_limit, date_changed, num_version) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)", [task.user_id, task.context_id, task.project_id, task.title, task.description, task.state, task.verification_list, task.important_fixed, task.date_added, task.date_completed, task.date_limit, date_changed, num_version], (err, resultTask)=>{
+                    if(err){
+                        throw Error("Error creating the task");
+                    }
+                    else{
+                        return resultTask.insertId;
+                    }
+                });
+            }
+        }
+    })
+
+}
+
+taskService.modificateTask = async (task)=>{
+    await db.query("", [], (err, res)=>{
+
+    })
+}
+
 module.exports = taskService;
