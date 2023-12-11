2167f7c4941378b660da5202ca078bbdcb080882
Pabluskagamo <pabluskagamo@gmail.com>
2023-12-11 18:30:13 +0100

    auth state

---
+++ Frontend/screens/auth/authState.js
@@ -0,0 +1,79 @@
+import React from "react";
+import AuthContext from "./authContext";
+
+
+const AuthState = props => {
+    const [state, dispatch] = React.useReducer(
+        (prevState, action) => {
+            switch (action.type) {
+                case 'RESTORE_TOKEN':
+                    return {
+                        ...prevState,
+                        userToken: action.token,
+                        isLoading: false,
+                    };
+                case 'SIGN_IN':
+                    return {
+                        ...prevState,
+                        isSignout: false,
+                        userToken: action.token,
+                    };
+                case 'SIGN_OUT':
+                    return {
+                        ...prevState,
+                        isSignout: true,
+                        userToken: null,
+                        isLoading: false
+                    };
+            }
+        },
+        {
+            isLoading: true,
+            isSignout: false,
+            userToken: null,
+        }
+    );
+
+    const authFunctions = React.useMemo(
+        () => ({
+            signIn: async (data) => {
+                // In a production app, we need to send some data (usually username, password) to server and get a token
+                // We will also need to handle errors if sign in failed
+                // After getting token, we need to persist the token using `SecureStore` or any other encrypted storage
+                // In the example, we'll use a dummy token
+
+                dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+            },
+            signOut: () => {
+                console.log("SINGING OUT")
+                dispatch({ type: 'SIGN_OUT' })
+            },
+            signUp: async (data) => {
+                // In a production app, we need to send user data to server and get a token
+                // We will also need to handle errors if sign up failed
+                // After getting token, we need to persist the token using `SecureStore` or any other encrypted storage
+                // In the example, we'll use a dummy token
+
+                dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+            },
+        }),
+        []
+    );
+
+    return (
+        <AuthContext.Provider
+            value={{
+                signIn: authFunctions.signIn,
+                signOut: authFunctions.signOut,
+                signUp: authFunctions.signUp,
+                isLoading: state.isLoading,
+                isSignout: state.isSignout,
+                userToken: state.userToken
+            }}
+        >
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthState;
