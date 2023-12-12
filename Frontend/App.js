9869c9070b5bc8e86008a8838415fb23cc4a9268
EXT96752 <DSI.GDT.CDSOA427@enagas.es>
2023-12-12 19:58:52 +0100

    Login provisional

--- Frontend/App.js
+++ Frontend/App.js
@@ -5,7 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import CustomButton from './components/buttons/Button';
 import SignInScreen from './screens/auth/login';
-// import AuthState from './screens/auth/authState';
+import AuthState from './screens/auth/authState';
 import AuthContext from './screens/auth/authContext';
 
 const Drawer = createDrawerNavigator();
@@ -19,49 +19,8 @@ function SplashScreen() {
   );
 }
 
-const HomeScreen = ({navigation}) => {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to TFG-GTD APP!</Text>
-      <CustomButton
-        text="Go to Login"
-        handler={() => navigation.navigate('Login')}
-      />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-export default function App() {
-  const [state, dispatch] = React.useReducer(
-    (prevState, action) => {
-      switch (action.type) {
-        case 'RESTORE_TOKEN':
-          return {
-            ...prevState,
-            userToken: action.token,
-            isLoading: false,
-          };
-        case 'SIGN_IN':
-          return {
-            ...prevState,
-            isSignout: false,
-            userToken: action.token,
-          };
-        case 'SIGN_OUT':
-          return {
-            ...prevState,
-            isSignout: true,
-            userToken: null,
-          };
-      }
-    },
-    {
-      isLoading: true,
-      isSignout: false,
-      userToken: null,
-    }
-  );
+function Router() {
+  const state = React.useContext(AuthContext);
 
   React.useEffect(() => {
     // Fetch the token from storage then navigate to our appropriate place
@@ -79,60 +38,58 @@ export default function App() {
   
       // This will switch to the App screen or Auth screen and this loading
       // screen will be unmounted and thrown away.
-      dispatch({ type: 'RESTORE_TOKEN', token: userToken });
+      state.signOut();
     };
   
     bootstrapAsync();
   }, []);
 
-  const authContext = React.useMemo(
-    () => ({
-      signIn: async (data) => {
-        // In a production app, we need to send some data (usually username, password) to server and get a token
-        // We will also need to handle errors if sign in failed
-        // After getting token, we need to persist the token using `SecureStore` or any other encrypted storage
-        // In the example, we'll use a dummy token
-
-        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
-      },
-      signOut: () => dispatch({ type: 'SIGN_OUT' }),
-      signUp: async (data) => {
-        // In a production app, we need to send user data to server and get a token
-        // We will also need to handle errors if sign up failed
-        // After getting token, we need to persist the token using `SecureStore` or any other encrypted storage
-        // In the example, we'll use a dummy token
-
-        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
-      },
-    }),
-    []
+  return (
+      <Drawer.Navigator>
+      {state.isLoading ? (
+          // We haven't finished checking for the token yet
+          <Drawer.Screen name="Splash" component={SplashScreen} />
+        ) : state.userToken == null ? (
+          // No token found, user isn't signed in
+          <Drawer.Screen
+            name="SignIn"
+            component={SignInScreen}
+            options={{
+              title: 'Sign in',
+              // When logging out, a pop animation feels intuitive
+              animationTypeForReplace: state.isSignout ? 'pop' : 'push',
+            }}
+          />
+        ) : (
+          // User is signed in
+          <Drawer.Screen name="Home" component={HomeScreen} />
+        )}
+      </Drawer.Navigator>
   );
+}
+
+const HomeScreen = ({navigation}) => {
+  const state = React.useContext(AuthContext);
   
   return (
-    <AuthContext.Provider value={authContext}>
+    <View style={styles.container}>
+      <Text>Welcome to TFG-GTD APP!</Text>
+      <CustomButton
+        text="Logout"
+        handler={() => state.signOut()}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <AuthState>
       <NavigationContainer>
-        <Drawer.Navigator>
-        {state.isLoading ? (
-            // We haven't finished checking for the token yet
-            <Drawer.Screen name="Splash" component={SplashScreen} />
-          ) : state.userToken == null ? (
-            // No token found, user isn't signed in
-            <Drawer.Screen
-              name="SignIn"
-              component={SignInScreen}
-              options={{
-                title: 'Sign in',
-                // When logging out, a pop animation feels intuitive
-                animationTypeForReplace: state.isSignout ? 'pop' : 'push',
-              }}
-            />
-          ) : (
-            // User is signed in
-            <Drawer.Screen name="Home" component={HomeScreen} />
-          )}
-        </Drawer.Navigator>
+        <Router/>
       </NavigationContainer>
-    </AuthContext.Provider>
+    </AuthState>
   );
 }
 
