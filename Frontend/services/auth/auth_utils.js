806ff64348b9f6d70aecace9cf5b7eae4ff1be19
Pabluskagamo <pabluskagamo@gmail.com>
2024-01-03 12:34:14 +0100

    Fix ultimo commit

--- Frontend/services/auth/auth_utils.js
+++ Frontend/services/auth/auth_utils.js
@@ -4,23 +4,23 @@ import tokenStorage from "./token_store/storage"
 let authtoken = null;
 
 const setAuthHeaders = async function (config) {
-    console.log("COMPROBAR SI AUTHTOKEN EXISTE", authtoken)
-    // if(!authtoken){
-    //     let token = await tokenStorage.getToken();
-    //     authtoken = token;
-    // }
+    
+    if(!authtoken){
+        let token = await tokenStorage.getToken();
+        authtoken = token;
+    }
 
-    // if(Date.now() > (Date.parse(authtoken.expires_at) ?? Date.now())){
-    //     const newToken = await authService.restoreToken(authtoken)
+    if(Date.now() > (Date.parse(authtoken.expires_at) ?? Date.now())){
+        const newToken = await authService.restoreToken(authtoken)
 
-    //     if(newToken){
-    //         token.expires_at = new Date(Date.now() + token.expires_in*1000);
-    //         await tokenStorage.storeToken(newToken);
-    //         authtoken = newToken;
-    //     }
-    // }
+        if(newToken){
+            token.expires_at = new Date(Date.now() + token.expires_in*1000);
+            await tokenStorage.storeToken(newToken);
+            authtoken = newToken;
+        }
+    }
 
-    // config.headers.Authorization = `Bearer ${authtoken.access_token}`;
+    config.headers.Authorization = `Bearer ${authtoken.access_token}`;
 
     return config;
 }
