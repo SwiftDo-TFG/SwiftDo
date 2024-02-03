728ca8f460433a99007015c22a9950952e865a58
Pabluskagamo <pabluskagamo@gmail.com>
2024-02-03 11:42:34 +0100

    [Back] nuevo endpoint task/info para sidebar

--- Backend/routes/task.js
+++ Backend/routes/task.js
@@ -13,19 +13,15 @@ router.get('/', async (req, res) => {
   console.log("[TASK FIND BY ID] The filters", filters)
 
   try {
-    // let task
-    // if(Object.keys(filters).length === 0){
-      // task = await taskService.findTaskByUserId(user_id);
-      const userInfo = await userService.findUserById(user_id)
-      const task_inbox = await taskService.getInfo(user_id, 1)
-      const task_ca = await taskService.getInfo(user_id, 2)
-      const task_prog = await taskService.getInfo(user_id, 3)
-      const task_arch = await taskService.getInfo(user_id, 4)
-    // }else{
-      // task = await taskService.findTasksByFilters(user_id, filters)
-    // }
+    let task;
 
-    res.send({userName: userInfo.name, task_inbox: task_inbox, task_ca: task_ca, task_prog: task_prog, task_arch: task_arch});
+    if(Object.keys(filters).length === 0){
+      task = await taskService.findTaskByUserId(user_id);
+    }else{
+      task = await taskService.findTasksByFilters(user_id, filters)
+    }
+
+    res.send(task);
   } catch (error) {
     console.log('[Exception]:', error.message)
     res.sendStatus(404);
@@ -50,10 +46,10 @@ router.post('/addTag', async (req, res) => {
   const tag = req.body.name;
   const id = req.body.task_id;
 
-  try{
-    const t = await taskService.addTag(id,tag);
+  try {
+    const t = await taskService.addTag(id, tag);
     res.send(t);
-  } catch (err){
+  } catch (err) {
     console.log('[Exception]:', err.message)
     res.sendStatus(409)
   }
@@ -80,6 +76,26 @@ router.get('/:id', async (req, res) => {
   try {
     const task = await taskService.findTaskById(id);
     res.send(task);
+  } catch (error) {
+    console.log('[Exception]:', error.message)
+    res.sendStatus(404);
+  }
+})
+
+
+router.get('/info', async (req, res) => {
+  const user_id = res.locals.oauth.token.user.id;
+  const filters = req.query;
+
+  try {
+    
+    const userInfo = await userService.findUserById(user_id)
+    const task_inbox = await taskService.getInfo(user_id, 1)
+    const task_ca = await taskService.getInfo(user_id, 2)
+    const task_prog = await taskService.getInfo(user_id, 3)
+    const task_arch = await taskService.getInfo(user_id, 4)
+  
+    res.send({ userName: userInfo.name, task_inbox: task_inbox, task_ca: task_ca, task_prog: task_prog, task_arch: task_arch });
   } catch (error) {
     console.log('[Exception]:', error.message)
     res.sendStatus(404);
