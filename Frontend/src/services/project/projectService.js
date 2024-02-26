81b513f6906d0b871bf40c77d9d55c5d3866f41d
Pabluskagamo <pabluskagamo@gmail.com>
2024-02-26 11:14:17 +0100

    [Front] Servicios cambio de URL

--- Frontend/src/services/project/projectService.js
+++ Frontend/src/services/project/projectService.js
@@ -4,7 +4,7 @@ import authUtils from "../auth/auth_utils"
 const instance = axios.create({
     // baseURL: 'http://localhost:3000',
     // baseURL: 'http://192.168.0.137:3000',
-    baseURL: 'http://ec2-16-171-198-23.eu-north-1.compute.amazonaws.com:3000',
+    baseURL: 'http://ec2-16-171-169-109.eu-north-1.compute.amazonaws.com:3000',
     timeout: 1000,
     // headers: { Authorization: `Bearer d04d34bd905b677bc04d205cf3c3a4e7d91601da` }
 });
