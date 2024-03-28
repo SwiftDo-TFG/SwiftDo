1f68f6e5e53dd4e0484f0cbc2f8c30552def58c0
CGLMaster <cgomez22@ucm.es>
2024-03-28 14:05:51 +0100

    Dejarlo con lo de pablo, para que se vea el context

--- Frontend/src/screens/tasks/actionScreen.jsx
+++ Frontend/src/screens/tasks/actionScreen.jsx
@@ -290,14 +290,12 @@ function ActionScreen(props) {
           {/* Filter Context / tag */}
           <View style={{ minWidth: 50, justifyContent: 'flex-end' }}>
             <TouchableOpacity style={styles.area} onPress={() => setIsFilterModalOpen(true)}>
-              <MaterialCommunityIcons name="filter-variant" size={28} color={Colors[theme].white} />
-
-              {/* AQUI IRIA EL TEXTO DEL CONTEXTO FILTRADO
+              {/* AQUI IRIA EL TEXTO DEL CONTEXTO FILTRADO */}
               {filterContext.isFiltered ? <ContextBadge context_name={filterContext.context_name} handlePress={() => {
                 // handleContextAction(null, context_name);
                 filterContext.clearFilter();
                 reloadData();
-              }} /> : <MaterialCommunityIcons name="filter-variant" size={28} color={Colors[theme].white} />} */}
+              }} /> : <MaterialCommunityIcons name="filter-variant" size={28} color={Colors[theme].white} />}
             </TouchableOpacity>
           </View>
         </View>
