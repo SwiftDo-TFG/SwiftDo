2486af00ce614c92c61c417d2b6754817214763e
JavierGil5 <javigi01@ucm.es>
2024-03-28 13:01:47 +0100

    gettags tagservice y tag

--- Backend/services/tagService.js
+++ Backend/services/tagService.js
@@ -22,7 +22,7 @@ tagService.findTag = async (tag) => {
     return res.rows[0];
 }
 
-tagService.getAllTagsByUser = async (id, search) => {
+tagService.getAllTagsByUserLimit = async (id, search) => {
     const res = await db.query('SELECT DISTINCT t.name, t.colour FROM tags t JOIN tagstotask tt ON t.name = tt.nametag JOIN tasks tk ON tk.task_id = tt.task_id WHERE tk.user_id = $1 AND LOWER(t.name) LIKE LOWER($2) ORDER BY t.name ASC LIMIT 5', [id, search])
 
     if (res.rows.length < 1) {
@@ -32,6 +32,13 @@ tagService.getAllTagsByUser = async (id, search) => {
     return res.rows;
 }
 
+tagService.getAllTagsByUser = async (id) => {
+    const res = await db.query('SELECT DISTINCT t.name, t.colour FROM tags t JOIN tagstotask tt ON t.name = tt.nametag JOIN tasks tk ON tk.task_id = tt.task_id WHERE tk.user_id = $1 ORDER BY t.name ASC', [id])
 
+    if (res.rows.length < 1) {
+        return false;
+    }
+    return res.rows;
+}
 
 module.exports = tagService;
