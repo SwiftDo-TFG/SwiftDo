7b0e35a815d4265662ae9c54275b3d35679d4a9f
Pabluskagamo <pabluskagamo@gmail.com>
2024-04-08 12:04:27 +0200

    [Back] Cambios en la expiracion de los refresh tokens

--- Backend/oauth/server.js
+++ Backend/oauth/server.js
@@ -5,6 +5,7 @@ module.exports = new OAuthServer({
   model: model,
   grants: ['authorization_code', 'refresh_token'],
   accessTokenLifetime: 60 * 60 * 24, // 24 hours, or 1 day
+  refreshTokenLifetime: 60 * 60 * 36, // 36 hours, or 1 day and a half
   allowEmptyState: true,
   allowExtendedTokenAttributes: true
 })
